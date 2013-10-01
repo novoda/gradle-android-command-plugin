9e75923abab66e24e2174f9e3ab33a31a49e6dfe
frankiesardo <fran.sardo@gmail.com>
2013-10-01 10:31:57 +0100

    Add sanity check after reading output from aapt

--- gradle-android-command-plugin/src/main/groovy/com/novoda/gradle/command/Apk.groovy
+++ gradle-android-command-plugin/src/main/groovy/com/novoda/gradle/command/Apk.groovy
@@ -15,6 +15,11 @@ class Apk extends Command {
         String output = "$aapt dump badging $apkPath".execute().text.readLines().find {
             it.startsWith("package:")
         }
+
+        if (output == null) {
+            throw new IllegalStateException("Impossible to find package for apk $apkPath")
+        }
+
         def matcher = output.readLines()[0]=~/name='(.*?)'/
         if (matcher) {
             matcher[0][1]
@@ -29,6 +34,11 @@ class Apk extends Command {
         String output = "$aapt dump badging $apkPath".execute().text.readLines().find {
             it.startsWith("launchable-activity:")
         }
+
+        if (output == null) {
+            throw new IllegalStateException("Impossible to find launchableActivity for apk $apkPath")
+        }
+
         def matcher = output.readLines()[0]=~/name='(.*?)'/
         if (matcher) {
             matcher[0][1]
