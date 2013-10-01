bb70f7b9e2470c37486aac439b97884513cdf2e3
frankiesardo <fran.sardo@gmail.com>
2013-10-01 10:01:14 +0100

    Clean up product flavors code

--- gradle-android-command-plugin/src/main/groovy/com/novoda/gradle/command/AndroidCommandPlugin.groovy
+++ gradle-android-command-plugin/src/main/groovy/com/novoda/gradle/command/AndroidCommandPlugin.groovy
@@ -8,47 +8,27 @@ public class AndroidCommandPlugin implements Plugin<Project> {
     @Override
     void apply(Project project) {
         def hasAppPlugin = project.plugins.hasPlugin com.android.build.gradle.AppPlugin
-        def hasLibraryPlugin = project.plugins.hasPlugin com.android.build.gradle.LibraryPlugin
-        def log = project.logger
 
-        // Ensure the Android plugin has been added in app or library form, but not both.
-        if (!hasAppPlugin && !hasLibraryPlugin) {
-            throw new IllegalStateException("The 'android' or 'android-library' plugin is required.")
-        } else if (hasAppPlugin && hasLibraryPlugin) {
-            throw new IllegalStateException(
-                    "Having both 'android' and 'android-library' plugin is not supported.")
+        if (!hasAppPlugin) {
+            throw new IllegalStateException("The 'android' plugin is required.")
         }
 
-        def variants = hasAppPlugin ? project.android.applicationVariants :
-                project.android.libraryVariants
-
-        project.android.metaClass.createTasks = { String name, type ->
+        project.android.metaClass.createTasks = { String name, Apk type ->
             String taskType = type.simpleName.capitalize()
+            String taskNamePrefix = name + taskType
 
-            variants.all { variant ->
-                // Get the build type name (e.g., "Debug", "Release").
+            project.android.applicationVariants.all { variant ->
                 def buildTypeName = variant.buildType.name.capitalize()
-                def projectFlavorNames = [""]
-                if (hasAppPlugin) {
-                    // Flavors are only available for the app plugin (e.g., "Free", "Paid").
-                    projectFlavorNames = variant.productFlavors.collect { it.name.capitalize() }
-                    // TODO support flavor groups... ugh
-                    if (projectFlavorNames.isEmpty()) {
-                        projectFlavorNames = [""]
-                    }
-                }
+                def projectFlavorNames = variant.productFlavors.collect { it.name.capitalize() }
                 def projectFlavorName = projectFlavorNames.join()
+                def variationName = projectFlavorName + buildTypeName
 
-                // The combination of flavor and type yield a unique "variation". This value is used for
-                // looking up existing associated tasks as well as naming the task we are about to create.
-                def variationName = "$projectFlavorName$buildTypeName"
-
-                Apk task = project.tasks.create(name + taskType + variationName, type)
+                Apk task = project.tasks.create(taskNamePrefix + variationName, type)
                 task.apkPath = variant.packageApplication.outputFile
             }
 
             project.tasks.matching {
-                it.name.startsWith "$name$taskType"
+                it.name.startsWith taskNamePrefix
             }
         }
     }
