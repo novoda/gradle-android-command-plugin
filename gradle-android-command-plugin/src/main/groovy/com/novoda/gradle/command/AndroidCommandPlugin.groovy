dc1e4a01deecb3fdb74784819419c1c6769cb458
Volker Leck <volker.leck@gmail.com>
2014-02-03 16:05:04 +0100

    use custom install task named 'push' instead of relying on android gradle plugin's install task

--- gradle-android-command-plugin/src/main/groovy/com/novoda/gradle/command/AndroidCommandPlugin.groovy
+++ gradle-android-command-plugin/src/main/groovy/com/novoda/gradle/command/AndroidCommandPlugin.groovy
@@ -11,8 +11,9 @@ public class AndroidCommandPlugin implements Plugin<Project> {
             throw new ProjectConfigurationException("The 'android' plugin is required.")
         }
         def extension = project.android.extensions.create("command", AndroidCommandPluginExtension, project)
-        extension.tasks "run", com.novoda.gradle.command.Run, ["install"]
-        extension.tasks "monkey", com.novoda.gradle.command.Monkey, ["install"]
-        extension.tasks "clearPrefs", com.novoda.gradle.command.ClearPreferences
+        extension.tasks "push", Install
+        extension.tasks "run", Run, ["push"]
+        extension.tasks "monkey", Monkey, ["push"]
+        extension.tasks "clearPrefs", ClearPreferences
     }
 }
