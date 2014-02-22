c74516135dafca68a1afef0b67404c2c29542ab1
Volker Leck <volker.leck@gmail.com>
2014-02-22 13:34:43 +0100

    fix task registration

--- gradle-android-command-plugin/src/main/groovy/com/novoda/gradle/command/AndroidCommandPlugin.groovy
+++ gradle-android-command-plugin/src/main/groovy/com/novoda/gradle/command/AndroidCommandPlugin.groovy
@@ -11,9 +11,9 @@ public class AndroidCommandPlugin implements Plugin<Project> {
             throw new ProjectConfigurationException("The 'android' plugin is required.")
         }
         def extension = project.android.extensions.create("command", AndroidCommandPluginExtension, project)
-        extension.tasks 'installDevice', Install, ['assemble']
-        extension.tasks 'run', Run, ['installDevice']
-        extension.tasks 'monkey', Monkey, ['installDevice']
-        extension.tasks 'clearPrefs', ClearPreferences
+        extension.task 'installDevice', Install, ['assemble']
+        extension.task 'run', Run, ['installDevice']
+        extension.task 'monkey', Monkey, ['installDevice']
+        extension.task 'clearPrefs', ClearPreferences
     }
 }
