759c51261e22513a855be9303963e1af602c32a2
Volker Leck <volker.leck@gmail.com>
2014-05-22 16:11:25 +0200

    extend sample to show off autoLogin via new scripting feature

--- sample/src/main/java/com/novoda/gradle/command/MainActivity.java
+++ sample/src/main/java/com/novoda/gradle/command/MainActivity.java
@@ -1,9 +1,12 @@
 package com.novoda.gradle.command;
 
-import android.os.Bundle;
 import android.app.Activity;
+import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends Activity {
 
@@ -11,12 +14,22 @@ public class MainActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        bindViews();
     }
 
+    private void bindViews() {
+        final TextView userNameView = (TextView) findViewById(R.id.input_username);
+        Button loginButton = (Button) findViewById(R.id.login_button);
+        loginButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(final View v) {
+                startActivity(HelloActivity.forUser(userNameView.getText().toString(), MainActivity.this));
+            }
+        });
+    }
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main, menu);
         return true;
     }
