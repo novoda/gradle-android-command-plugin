de0bd7c1f7d22ca76425dfa6e057cef42e6d2b5a
Emanuele Zattin <emanuelez@gmail.com>
2015-02-13 12:40:55 +0100

    Add support for specifying a seed for the monkey command

--- gradle-android-command-plugin/src/main/groovy/com/novoda/gradle/command/Monkey.groovy
+++ gradle-android-command-plugin/src/main/groovy/com/novoda/gradle/command/Monkey.groovy
@@ -5,6 +5,7 @@ import org.gradle.api.tasks.TaskAction
 class Monkey extends AdbTask {
 
     def events
+    def seed
 
     protected handleCommandOutput(def text) {
         super.handleCommandOutput(text)
@@ -14,12 +15,21 @@ class Monkey extends AdbTask {
 
     @TaskAction
     void exec() {
-        assertDeviceAndRunCommand(['shell', 'monkey', '-p', packageName, '-v', getEvents()])
+        def arguments = ['shell', 'monkey', '-p', packageName, '-v', getEvents()]
+        if (getSeed())
+            arguments += ['-s', getSeed()]
+        assertDeviceAndRunCommand(arguments)
     }
 
     private getEvents() {
         if (events instanceof Closure)
             events = events.call()
         events ?: pluginEx.events
+    }
+
+    private getSeed() {
+        if (seed instanceof Closure)
+            seed = seed.call()
+        seed ?: pluginEx.getSeed()
     }
 }
