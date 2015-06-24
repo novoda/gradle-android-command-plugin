929f6ce09dd7cb3299b73381acf20a4a4c8aeb33
Friedger Müffke <friedger@gmail.com>
2015-06-25 00:57:35 +0200

    add ActivityStack task see #65

---
+++ gradle-android-command-plugin/src/main/groovy/com/novoda/gradle/command/ActivityStack.groovy
@@ -0,0 +1,35 @@
+package com.novoda.gradle.command
+
+class ActivityStack extends AdbTask {
+
+    protected handleCommandOutput(def text) {
+        super.handleCommandOutput(text)
+
+        def activityRecords = []
+        text.eachLine { line ->
+            def matcher = line =~ /Run #([0-9]*): ActivityRecord\{\S* \S* ([^\/]*)\/(\S*)/
+            if (matcher) {
+                activityRecords << new ActivityRecord(index:matcher[0][1], packageName:matcher[0][2], activityName:matcher[0][3])
+            }
+        }
+        activityRecords.each {
+            logger.info it.toString()
+        }
+    }
+
+    def getActivityRecords() {
+        def commandLine = ['shell', 'dumpsys', 'activity', '|', 'grep', '-i', 'run']
+        AdbCommand command = [adb: pluginEx.getAdb(), deviceId: getDeviceId(), parameters: commandLine]
+        logger.info "running command: $command"
+        def output = command.execute().text
+
+        def activityRecords = []
+        output.eachLine { line ->
+            def matcher = line =~ /Run #([0-9]*): ActivityRecord\{\S* \S* ([^\/]*)\/(\S*)/
+            if (matcher) {
+                activityRecords << new ActivityRecord(index: matcher[0][1], packageName: matcher[0][2], activityName: matcher[0][3])
+            }
+        }
+        activityRecords
+    }
+}
