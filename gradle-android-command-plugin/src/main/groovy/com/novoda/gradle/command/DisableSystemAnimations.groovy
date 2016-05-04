d494f91a2cfb00b9104facfdbd0db8b8233af924
Sebastian Engel <engel.sebastian@gmail.com>
2016-05-04 14:12:45 +0200

    Add task to disable system animations (setting window_animation_scale, transition_animation_scale and animator_duration_scale to 0).

---
+++ gradle-android-command-plugin/src/main/groovy/com/novoda/gradle/command/DisableSystemAnimations.groovy
@@ -0,0 +1,30 @@
+package com.novoda.gradle.command
+
+import org.gradle.api.tasks.TaskAction
+
+class DisableSystemAnimations extends AdbTask {
+
+  @TaskAction
+  void exec() {
+    assertDeviceConnected()
+    disableAnimatorDuration()
+    disableTransitionAnimations()
+    disableWindowAnimations()
+  }
+
+  private disableWindowAnimations() {
+    def arguments = ['shell', 'settings', 'put', 'global', 'window_animation_scale', '0']
+    runCommand(arguments)
+  }
+
+  private disableTransitionAnimations() {
+    def arguments = ['shell', 'settings', 'put', 'global', 'transition_animation_scale', '0']
+    runCommand(arguments)
+  }
+
+  private disableAnimatorDuration() {
+    def arguments = ['shell', 'settings', 'put', 'global', 'animator_duration_scale', '0']
+    runCommand(arguments)
+  }
+
+}
