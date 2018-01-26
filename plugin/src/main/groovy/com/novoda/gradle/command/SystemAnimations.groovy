2b71cc34bd7361d058493a50cb1e1701a6d8ed53
Said Tahsin Dane <tahsin@novoda.com>
2018-01-26 01:13:40 +0100

    Fix SystemAnimations formatting

--- plugin/src/main/groovy/com/novoda/gradle/command/SystemAnimations.groovy
+++ plugin/src/main/groovy/com/novoda/gradle/command/SystemAnimations.groovy
@@ -4,29 +4,29 @@ import org.gradle.api.tasks.TaskAction
 
 class SystemAnimations extends AdbTask {
 
-  Boolean enable;
+    boolean enable
 
-  @TaskAction
-  void exec() {
-    assertDeviceConnected()
-    windowAnimations()
-    transitionAnimations()
-    animatorDuration()
-  }
+    @TaskAction
+    void exec() {
+        assertDeviceConnected()
+        windowAnimations()
+        transitionAnimations()
+        animatorDuration()
+    }
 
-  private windowAnimations() {
-    def arguments = ['shell', 'settings', 'put', 'global', 'window_animation_scale', enable ? '1' : '0']
-    runCommand(arguments)
-  }
+    private windowAnimations() {
+        def arguments = ['shell', 'settings', 'put', 'global', 'window_animation_scale', enable ? '1' : '0']
+        runCommand(arguments)
+    }
 
-  private transitionAnimations() {
-    def arguments = ['shell', 'settings', 'put', 'global', 'transition_animation_scale', enable ? '1' : '0']
-    runCommand(arguments)
-  }
+    private transitionAnimations() {
+        def arguments = ['shell', 'settings', 'put', 'global', 'transition_animation_scale', enable ? '1' : '0']
+        runCommand(arguments)
+    }
 
-  private animatorDuration() {
-    def arguments = ['shell', 'settings', 'put', 'global', 'animator_duration_scale', enable ? '1' : '0']
-    runCommand(arguments)
-  }
+    private animatorDuration() {
+        def arguments = ['shell', 'settings', 'put', 'global', 'animator_duration_scale', enable ? '1' : '0']
+        runCommand(arguments)
+    }
 
 }
